3c9e0f5766cf080139eb312cd3ddbea47aa48ddd
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-16 20:30:44 -0300

    Fixing scenario 18

--- technical-fundamentals/coding/problems/18_intersection.ts
+++ technical-fundamentals/coding/problems/18_intersection.ts
@@ -14,4 +14,16 @@ export type Node<T> = {
 export default function intersection<T>(
   list1: Node<T> | undefined,
   list2: Node<T> | undefined,
-): Node<T> | undefined {}
+): Node<T> | undefined {
+  const map: Map<Node<T>, boolean> = new Map<Node<T>, boolean>();
+  const lList1 = new LinkedList(list1);
+  lList1.visit((n) => {
+    map.set(n, true);
+  });
+  let p = list2;
+  while(p){
+    if(map.has(p)) return p;
+    p = p.next;
+  }
+
+}
