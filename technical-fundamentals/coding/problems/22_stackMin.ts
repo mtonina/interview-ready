ab3f85426b0584ad1b200e4c4a590a7c8776880e
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-17 14:12:45 -0300

    Fixing scenario 22

--- technical-fundamentals/coding/problems/22_stackMin.ts
+++ technical-fundamentals/coding/problems/22_stackMin.ts
@@ -5,19 +5,34 @@
 //
 
 export default class StackMin<T> {
-    constructor() {
+    private array: T[];
+    private minArray: T[];
 
+    constructor() {
+        this.array = [];
+        this.minArray = []; //it has always the min value at some point
     }
 
     push(value: T): void {
+        this.array.push(value);
+        if(this.minArray.length === 0 || value <= this.peek()){
+            this.minArray.push(value);
+        }
+    }
 
+    private peek() {
+        return this.minArray[this.minArray.length - 1];
     }
 
     pop(): T | undefined {
-
+        const result = this.array.pop();
+        if(this.peek() === result){
+            this.minArray.pop();
+        }
+        return result;
     }
 
     min(): T | undefined {
-
+        return this.peek();
     }
 }
