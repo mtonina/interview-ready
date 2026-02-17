9a8e5a9c6a97ac42047e81c7a46c826e0bc3d01a
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-17 17:52:26 -0300

    Fixing scenario 26

--- technical-fundamentals/coding/problems/26_animalShelter.ts
+++ technical-fundamentals/coding/problems/26_animalShelter.ts
@@ -10,6 +10,9 @@
 // such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
 // You may use the built-in LinkedList data structure.
 
+import './10_LinkedList'
+import {LinkedList} from "./10_LinkedList";
+
 export type AnimalType = "dog" | "cat";
 
 export class Animal {
@@ -21,21 +24,46 @@ export class Animal {
 
 export default class AnimalShelter {
 
+    private list = new LinkedList<Animal>();
+
     constructor() {
     }
 
     enqueue(type: AnimalType): void {
-
+        this.list.push(new Animal(type));
     }
 
     dequeueAny(): Animal | undefined {
-
+        if(!this.list.head) return undefined;
+        let ret = this.list.head?.value;
+        this.list.head = this.list.head.next;
+        return ret;
     }
 
     dequeueDog(): Animal | undefined {
+        return this.dequeueByParam("dog");
     }
 
     dequeueCat(): Animal | undefined {
+        return this.dequeueByParam("cat");
+    }
+
+    private dequeueByParam(animal: AnimalType) {
+        let p = this.list.head;
+        if (!p) return p;
+        if (p?.value.type === animal) {
+            return this.dequeueAny();
+        } else {
+            while (p && p.next && p.next.value.type !== animal) {
+                p = p.next;
+            }
+            if (p.next && p.next.value.type === animal) {
+                let ret = p.next.value;
+                p.next = p.next.next;
+                return ret;
+            }
+        }
+        return undefined;
     }
 }
 
