3bc8d5ca81fb5da4681688bf2b28158b4cbb294f
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-17 16:49:50 -0300

    Fixing scenario 24

--- technical-fundamentals/coding/problems/24_queueViaStacks.ts
+++ technical-fundamentals/coding/problems/24_queueViaStacks.ts
@@ -3,18 +3,41 @@
 // Implement a MyQueue class which implements a queue using two stacks.
 
 export default class MyQueue<T> {
+    private orderedStack: T[] = [];
+    private queueStack: T[] = [];
+
     constructor() {
     }
 
     enqueue(value: T): void {
+        this.orderedStack.push(value);
     }
 
     dequeue(): T | undefined {
+        if(this.queueStack.length > 0){
+            return this.queueStack.pop();
+        } else {
+            if(this.orderedStack.length === 0) return undefined;
+            while(this.orderedStack.length > 0){
+                this.queueStack.push(this.orderedStack.pop()!);
+            }
+            return this.queueStack.pop();
+        }
     }
 
     peek(): T | undefined {
+        if(this.queueStack.length > 0){
+            return this.queueStack[this.queueStack.length - 1];
+        } else {
+            if(this.orderedStack.length === 0) return undefined;
+            while(this.orderedStack.length > 0){
+                this.queueStack.push(this.orderedStack.pop()!);
+            }
+            return this.queueStack[this.queueStack.length - 1];
+        }
     }
 
     isEmpty(): boolean {
+        return this.queueStack.length === 0 && this.orderedStack.length === 0;
     }
 }
