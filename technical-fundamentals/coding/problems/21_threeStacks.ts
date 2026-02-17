fa8eaec6ddc31f845b1821e9bd185802defc0db0
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-17 13:06:29 -0300

    Fixing scenario 21 with different approach

--- technical-fundamentals/coding/problems/21_threeStacks.ts
+++ technical-fundamentals/coding/problems/21_threeStacks.ts
@@ -1,13 +1,11 @@
 // 1. *Three in One*: Describe how you could use a single array to implement three stacks.
 
+//this approach is different, but it has same complexity than other solution... so I keep it
 export default class ThreeStacks<T> {
-    private array: T[];
+    private array: Array<T | undefined>;
     private index0 = 0;
     private index1 = 1;
     private index2 = 2;
-    //0, 3, 6, 9
-    //1, 4, 7, 10
-    //2, 5, 8, 11
 
     constructor(arrayLength: number) {
         this.array = new Array(arrayLength).fill(null);
@@ -27,15 +25,18 @@ export default class ThreeStacks<T> {
     }
 
     pop(stackNum: number): T | undefined {
-        let value: T;
+        let value: T | undefined;
         if(stackNum % 3 === 0){
             value = this.array[this.index0 - 3];
+            this.array[this.index0 - 3] = undefined; //to keep consistency. Should not be necessary
             this.index0 -= 3;
         } else if(stackNum % 3 === 1){
             value = this.array[this.index1 - 3];
+            this.array[this.index1 - 3] = undefined;
             this.index1 -= 3;
         } else if(stackNum % 3 === 2){
             value = this.array[this.index2 - 3];
+            this.array[this.index2 - 3] = undefined;
             this.index2 -= 3;
         } else {
             return undefined;
@@ -44,7 +45,7 @@ export default class ThreeStacks<T> {
     }
 
     peek(stackNum: number): T | undefined {
-        let value: T;
+        let value: T | undefined;
         if(stackNum % 3 === 0){
             value = this.array[this.index0 - 3];
         } else if(stackNum % 3 === 1){
