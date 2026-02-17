2b547717e78a9e0c8392eec1cf3320acef813f1e
Marcos Tonina <marcos.tonina@outlook.com>
2026-02-17 17:11:04 -0300

    Fixing scenario 25

--- technical-fundamentals/coding/problems/25_sortStack.ts
+++ technical-fundamentals/coding/problems/25_sortStack.ts
@@ -6,18 +6,42 @@
 // The stack supports the following operations: push, pop, peek, and isEmpty.
 
 export default class SortStack<T> {
+    private stack: T[] = [];
+
     constructor() {
     }
-
     push(value: T): void {
+        if(this.stack.length === 0 || value <= this.peek()){
+            this.stack.push(value);
+        } else {
+            let pop: T | undefined = this.stack.pop();
+            let aux: T[] = [];
+            while(pop && pop < value){
+                aux.push(pop);
+                pop = this.stack.pop();
+            }
+            if(pop && pop > value) this.stack.push(pop);
+            this.stack.push(value);
+            this.getAllAux(aux);
+        }
+    }
+
+    private getAllAux(aux: T[]): void{
+        let pop;
+        while(pop = aux.pop()){
+            this.stack.push(pop);
+        }
     }
 
     pop(): T | undefined {
+        return this.stack.pop();
     }
 
-    peek(): T | undefined {
+    peek(): T {
+        return this.stack[this.stack.length - 1];
     }
 
     isEmpty(): boolean {
+        return this.stack.length === 0;
     }
 }
